c643bab3b23ecd716cee3ead0345a87d6f914173
Анатолий <Chepanov199287@gmail.com>
2023-06-22 19:23:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SkillFactory_15_6_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                var classes = new[]
+                {
+                    new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+                    new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+                    new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+                };
+                var allStudents = GetAllStudents(classes);
+
+                Console.WriteLine(string.Join(" ", allStudents));
+            }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var studentsList = classes.SelectMany(s => s.Students).ToArray();
+
+            return studentsList;
+        }
+
+        public class Classroom
+            {
+                public List<string> Students = new List<string>();
+
+            public object Name { get; internal set; }
+        }       
+    
+    }
+}
